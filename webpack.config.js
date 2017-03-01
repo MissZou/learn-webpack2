b496306885aaa29a1dcf6761b22f3181c3555de3
weiyun.zwy <weiyun.zwy@alibaba-inc.com>
2017-03-01 18:01:40 +0800

    css code splitting

---
+++ webpack.config.js
@@ -0,0 +1,19 @@
+var path = require('path');
+var ExtractTextPlugin = require("extract-text-webpack-plugin");
+
+module.exports = {
+  entry: './index.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist')
+  },
+  module: {
+    rules: [{
+      test: /\.css$/,
+      use: ExtractTextPlugin.extract({
+        use: 'css-loader'
+      })
+    }]
+  },
+  plugins: [new ExtractTextPlugin('style.css')]
+};
